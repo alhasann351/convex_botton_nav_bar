6c2bb46311c22f98339ed8dabe132fb4917faddd
alhasann351 <alhasann352@gmail.com>
2024-02-07 06:26:03 +0600

    nav bar added

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,3 +1,4 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -8,8 +9,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentTab = 2;
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: ConvexAppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        color: Colors.white,
+        height: 60,
+        //curveSize: 70,
+        style: TabStyle.reactCircle,
+        initialActiveIndex: currentTab,
+        items: const [
+          TabItem(
+            icon: Icons.chat_outlined,
+            title: 'Chat',
+          ),
+          TabItem(icon: Icons.person_2_outlined, title: 'Profile',),
+          TabItem(icon: Icons.dashboard_outlined, title: 'Dashboard',),
+          TabItem(icon: Icons.favorite_outline, title: 'Favorite',),
+          TabItem(icon: Icons.settings_outlined, title: 'Setting',),
+        ],
+      ),
+    );
   }
 }
